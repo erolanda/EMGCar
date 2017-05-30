b559dd651c093382b3669c222763efc2a09cc3f5
erolanda <elmaildemlanda@gmail.com>
2017-05-30 08:39:29 -0500

    first commit

---
+++ Assets/myScripts/ardControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class ardControl : MonoBehaviour {
+    private CarController m_Car;
+    public SerialController serialController;
+
+    private void Awake()
+    {
+        // get the car controller
+        m_Car = GetComponent<CarController>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        //serialController.SendSerialMessage("a");
+        string message = serialController.ReadSerialMessage();
+        Debug.Log(message);
+
+        if (message == null)
+            return;
+        else
+        {
+            float value = 0;
+            if(float.TryParse(message, out value))
+            {
+                if (value == 0f)
+                    m_Car.Move(0.5f, 0.4f, 0.3f, 0f);
+                else if (value == 1f)
+                    m_Car.Move(-0.5f, 0.4f, 0.3f, 0f);
+            }
+        }
+    }
+}
---
+++ Assets/myScripts/timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timer : MonoBehaviour {
+    private string text_time;
+    public float start_timer;
+    private float time;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update () {
+        if(start_timer > 0){
+            time += Time.deltaTime;
+            var minutes = time / 60;
+            var seconds = time % 60;
+            var fraction = (time * 100) % 100;
+            text_time = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+            Debug.Log(text_time);
+        }
+    }
+}
